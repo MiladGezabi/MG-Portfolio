9e11ba9312e051d414bb100de40d63607b1dbe97
MiladGezabi <milad.gezabi@gmail.com>
2023-06-28 21:52:17 +0200

    made hamburgermenu

--- Milad-Gezabi-Portfolio/src/components/Header.jsx
+++ Milad-Gezabi-Portfolio/src/components/Header.jsx
@@ -1,37 +1,48 @@
-import '../components-style/Header.css'
+import { useState } from "react";
+import "../components-style/Header.css";
+import Nav from "../sub-components/Nav";
+import BurgerNav from "../sub-components/BurgerNav";
 
-function Header () {
+function Header() {
+  const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
+  const [menu_class, setMenuClass] = useState("menu hidden");
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
 
-    return (
-        <section className="header">
-            <div className="name-box">
-                <a href="">
-                    <h1>
-                        Milad Gezabi
-                    </h1>
-                </a>
-            </div>
+  const updateMenu = () => {
+    if (!isMenuClicked) {
+      setBurgerClass("burger-bar clicked");
+      setMenuClass("menu visible");
+    } else {
+      setBurgerClass("burger-bar unclicked");
+      setMenuClass("menu hidden");
+    }
+    setIsMenuClicked(!isMenuClicked);
+  };
 
-            <nav>
-                <ul>
-                    <li>
-                        <a href=""> Home </a>
-                    </li>
-                    <li>
-                        <a href=""> About </a>
-                    </li>
-                    <li>
-                        <a href=""> Projects </a>
-                    </li>
-                    <li>
-                        <a href=""> Contact </a>
-                    </li>
-                </ul>
-            </nav>
-        </section>
-    )
+  return (
+    <section className="header">
+      <div className="name-box">
+        <a href="">
+          <h1>Milad Gezabi</h1>
+        </a>
+      </div>
 
-    
+      <Nav />
+
+          <div className="burger-menu" onClick={updateMenu}>
+            <div className={burger_class}></div>
+            <div className={burger_class}></div>
+            <div className={burger_class}></div>
+          </div>
+        
+
+        <div className={menu_class}>
+            <BurgerNav />
+        </div>
+      
+
+    </section>
+  );
 }
 
-export default Header
+export default Header;
