ce55d476b1de177ca4dfab84361cb6e509038cef
MiladGezabi <milad.gezabi@gmail.com>
2023-07-11 16:32:10 +0200

    added a project in the projects list

--- Milad-Gezabi-Portfolio/src/sub-components/projects.js
+++ Milad-Gezabi-Portfolio/src/sub-components/projects.js
@@ -1,10 +1,23 @@
+import cssLogo from '../assets/skill-logo/css.png';
+import expressLogo from '../assets/skill-logo/expressjs.png';
+import htmlLogo from '../assets/skill-logo/html.png';
+import jsLogo from '../assets/skill-logo/javascript.png';
+import nodejsLogo from '../assets/skill-logo/nodejs.png';
+import reactLogo from '../assets/skill-logo/react.png';
+import gitLogo from '../assets/skill-logo/git.png';
+import agileLogo from '../assets/skill-logo/agile.png';
+import figmaLogo from '../assets/skill-logo/figma.svg';
+import lowLogo from '../assets/skill-logo/database.png';
+import pokemon from '../assets/projects-pics/pokemon.png'
+
 export const projects = [
     {
-      "picture": "",
-      "name": "",
-      "description": "",
-      "siteLink": "",
-      "githubLink": "",
-      "made with": ["", ""]
+      "picture": pokemon,
+      "name": "Pokemon Teams",
+      "description": "Detta är ett simpelt javascript project, data hämtar från ett api och visas i en grid layout. Jag har också lagt till funktioner som: sökning, fånga pokemons, lägga till dem i sitt lag, ge de eget namn mm.",
+      "descriptionEN": "This is a simple javascript project, data fetches from an api and is displayed in a grid layout. I have also added functions such as: search, catch pokemons, add them to their team, give them their own name, etc.",
+      "siteLink": "https://miladgezabi.github.io/pokemonteam/",
+      "githubLink": "https://github.com/MiladGezabi/pokemonteam",
+      "made with": [htmlLogo, cssLogo, jsLogo]
     },
 ]
