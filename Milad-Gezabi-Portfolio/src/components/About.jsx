59b0c6ba574888bc9d80342d76eab193cbc066df
MiladGezabi <milad.gezabi@gmail.com>
2023-09-06 16:26:13 +0200

    added profile pic and did some finishin touches

--- Milad-Gezabi-Portfolio/src/components/About.jsx
+++ Milad-Gezabi-Portfolio/src/components/About.jsx
@@ -1,6 +1,7 @@
 import "../components-style/About.css";
 import { useRecoilState } from "recoil";
 import { LangState } from "./Atoms.js";
+import profilePic from "../assets/image0.jpeg";
 
 function About() {
   const [toggleLang, setToggleLang] = useRecoilState(LangState);
@@ -11,20 +12,38 @@ function About() {
         <>
           <h2>About me</h2>
 
-          <div className="image-box"></div>
+          <div className="about-inner">
+            <div className="image-box">
+              <div className="avatar">
+                <img src={profilePic} alt={profilePic} />
+              </div>
+            </div>
 
-          <div className="text-box">
-            Hello! My name is Milad Gezabi. I am UX/UI  <br /> designer and Front-end web developer from <br /> Karlstad, Sweden. I´m passionate about learning <br /> new technologies and perfecting the ones i know.
+            <div className="text-box">
+              Hello! My name is Milad Gezabi. I am UX/UI <br /> designer and
+              Front-end web developer from <br /> Karlstad, Sweden. I´m
+              passionate about learning <br /> new technologies and perfecting
+              the ones i know.
+            </div>
           </div>
         </>
       ) : (
         <>
           <h2>Om mig</h2>
 
-          <div className="image-box"></div>
+          <div className="about-inner">
+            <div className="image-box">
+              <div className="avatar">
+                <img src={profilePic} alt={profilePic} />
+              </div>
+            </div>
 
-          <div className="text-box">
-          Hej! Jag heter Milad Gezabi. Jag är UX/UI-designer <br /> och Front-end webbutvecklare från Karlstad, Sverige. Jag brinner <br /> för att lära mig nya tekniker och att förbättra de jag redan känner.
+            <div className="text-box">
+              Hej! Jag heter Milad Gezabi. Jag är UX/UI-designer <br /> och
+              Front-end webbutvecklare från Karlstad, Sverige. Jag brinner{" "}
+              <br /> för att lära mig nya tekniker och att förbättra de jag
+              redan känner.
+            </div>
           </div>
         </>
       )}
